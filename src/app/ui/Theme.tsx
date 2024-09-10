da6c48706d9a99179f5bde63fabd3abcd53d5c5b
Phurin Nararat <phurin.nrt@gmail.com>
2024-09-10 09:55:39 +0700

    Update Theme.tsx
    Update: when change theme the theme icon will rotate 180deg

--- src/app/ui/Theme.tsx
+++ src/app/ui/Theme.tsx
@@ -1,16 +1,26 @@
 'use client';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
-const Theme = ({className}: {className: string}) => (
-    <button
-    type="button"
-    onClick={() => document.documentElement.classList.toggle('dark')}
-    className={`${className} rounded-full flex justify-center items-center`}
-    >
-        <FontAwesomeIcon icon={faSun} className='absolute opacity-100 dark:opacity-0 size-full' />
-        <FontAwesomeIcon icon={faMoon} className='absolute opacity-0 dark:opacity-100 size-full' />
-    </button>
-);
+const Theme = ({className}: {className: string}) => {
+    const [isRotated, setIsRotated] = useState(false);
+
+    const toggleTheme = () => {
+        document.documentElement.classList.toggle('dark');
+        setIsRotated(!isRotated);
+    };
+
+    return (
+        <button
+        type="button"
+        onClick={toggleTheme}
+        className={`${className} rounded-full flex justify-center items-center transition-transform duration-500 ${isRotated ? 'rotate-180' : ''}`}
+        >
+            <FontAwesomeIcon icon={faSun} className='absolute opacity-100 dark:opacity-0 size-full' />
+            <FontAwesomeIcon icon={faMoon} className='absolute opacity-0 dark:opacity-100 size-full' />
+        </button>
+    );
+};
 
 export default Theme;
