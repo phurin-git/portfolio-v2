6bc5f2060baa6510fc6efd7dcf1184a0b3a44f32
Phurin Nararat <phurin.nrt@gmail.com>
2024-09-10 19:57:56 +0700

    Update Projects.tsx

--- src/app/ui/Projects.tsx
+++ src/app/ui/Projects.tsx
@@ -18,11 +18,11 @@ const getCard = (data: {
         href: string;
     }[];
 }, index: number) => (
-    <div key={index} className='h-full rounded-lg flex flex-col overflow-hidden border dark:border-white/20 hover:shadow-lg transition-all ease-out duration-300'>
+    <div key={index} className='rounded-lg flex flex-col overflow-hidden border dark:border-white/20 hover:shadow-lg transition-all ease-out duration-300'>
         <Link href={data.href} title={data.title} target='_blank' rel='noopener noreferrer' className='relative w-full h-40'>
             <Image src={data.image} alt='' fill className='object-cover object-top mx-auto'/>
         </Link>
-        <div className='flex grow flex-col space-y-1 px-2'>
+        <div className='flex flex-col grow space-y-1 px-2'>
             <h3 className='font-semibold text-base tracking-tight mt-1'>{data.title}</h3>
             <time className='text-xs range'>{data.dates}</time>
             <p className='text-xs text-wrap opacity-80'>{data.description}</p>
@@ -32,7 +32,7 @@ const getCard = (data: {
                 <div key={index} className='rounded-md items-center font-semibold text-xs text-black dark:text-white bg-gray-400/20 hover:bg-gray-400/15 dark:bg-white/20 hover:dark:bg-white/15 px-2 py-1 transition-colors'>{tech}</div>
             ))}
         </div>
-        <div className='flex items-center p-2 gap-1'>
+        <div className='flex items-center gap-1 p-2'>
             {data.links.map((link, index) => (
                 <Link key={index} title={link.type} href={link.href} target='_blank' rel='noopener noreferrer' className='flex flex-wrap gap-1 items-start'>
                     <div className='rounded-md flex gap-2 items-center border font-semibold text-xs text-white dark:text-black bg-black dark:bg-white transition-colors shadow px-2 py-1'>
@@ -52,7 +52,7 @@ const Projects = () => (
             <span className='text-3xl font-bold tracking-tighter sm:text-5xl'>Check out my latest work</span>
             <p className='text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-center'>{`I've worked on a variety of projects, from simple websites to complex web applications. Here are a few of my favorites.`}</p>
         </div>
-        <div className='grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto'>
+        <div className='max-w-[800px] grid grid-cols-1 sm:grid-cols-2 gap-3 mx-auto'>
             {data.projects.map(getCard)}
         </div>
     </section>
